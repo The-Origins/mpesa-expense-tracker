0224df370c8e02b876e3a314ad471220de489228
Joshua <jwegahub@gmail.com>
2025-01-05 21:51:43 +0300

    updated register logic

--- controllers/user/auth/register.js
+++ controllers/user/auth/register.js
@@ -5,6 +5,31 @@ const issueJwt = require("../../../utils/user/auth/issueJwt");
 module.exports = async (req, res, next) => {
   try {
     const { name, phone, email, password } = req.body;
+
+    let existingEmailUser = await db
+      .collection("users")
+      .where("email", "==", email)
+      .limit(1)
+      .get();
+    
+    if(!existingEmailUser.empty)
+    {
+      res.code = 400
+      return next(new Error(`A user with these credentials already exists`))
+    }
+
+    let existingPhoneUser = await db
+      .collection("users")
+      .where("phone.number", "==", phone.number)
+      .limit(1)
+      .get();
+    
+    if(!existingPhoneUser.empty)
+    {
+      res.code = 400
+      return next(new Error(`A user with these credentials already exists`))
+    }
+
     const generatedHash = passwordUtils.generateHash(password);
     const userData = {
       name,
